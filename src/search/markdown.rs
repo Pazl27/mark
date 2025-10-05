48c3514af111dd97a153fc011b56437cd022027d
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-10-05 14:00:05 +0200

    save created time

--- src/search/markdown.rs
+++ src/search/markdown.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Local};
+
 use crate::error::Result;
 use std::{io::Read, path::PathBuf};
 
@@ -5,6 +7,7 @@ pub struct MarkdownFile {
     pub path: PathBuf,
     pub name: String,
     pub content: Option<String>,
+    pub created_at: Option<String>,
 }
 
 impl MarkdownFile {
@@ -15,10 +18,19 @@ impl MarkdownFile {
             .unwrap_or("unknown")
             .to_string();
 
+        let created_at = std::fs::metadata(&path)
+            .and_then(|meta| meta.created())
+            .ok()
+            .and_then(|time| {
+                let datetime: DateTime<Local> = time.into();
+                Some(datetime.format("%Y-%m-%d %H:%M:%S").to_string())
+            });
+
         Self {
             path: path.clone(),
             name,
             content: None,
+            created_at,
         }
     }
 
